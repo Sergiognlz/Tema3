43ed0e24543f492c86bfc640aca72bc747b70ce1
sgonzalez <sgonzalez@106-03.info2>
2024-11-12 14:40:20 +0100

    1 bidi

---
+++ Tema3/src/boletin3bidimen/Ejercicio1.java
@@ -0,0 +1,28 @@
+package boletin3bidimen;
+
+public class Ejercicio1 {
+	/*
+	 * Define un array de números enteros de 3 filas por 6 columnas con nombre num y
+	 * asigna los valores según la siguiente tabla. Muestra el contenido de todos
+	 * los elementos del array dispuestos en forma de tabla como se muestra en la
+	 * figura.
+	 */
+
+	public static void main(String[] args) {
+		//creamos tabla bidimensional
+		int tabla[][]=new int[3][6];
+		
+		//asignamos valores a las posiciones
+				tabla[0][1]=30;
+				tabla[0][2]=2;
+				tabla[0][6]=
+				tabla[][]=
+				tabla[][]=
+				tabla[][]=
+				tabla[][]=
+				tabla[][]=
+				tabla[][]=
+				tabla[][]=
+
+	}
+}
