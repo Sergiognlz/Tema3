92c12986c11e3dc2fda0f5a3bb8874c201cc97ab
sgonzalez <sgonzalez@106-03.info2>
2024-12-17 14:02:04 +0100

    13

--- Tema3/src/boletinstring/Ejercicio13.java
+++ Tema3/src/boletinstring/Ejercicio13.java
@@ -27,16 +27,38 @@ public class Ejercicio13 {
 		//guardamos  palabra2
 		palabra2=sc.next();
 		
+		//llamamos a la función y si el booleano anagrama es true entrará
 		if(anagrama(palabra1, palabra2)) {
-			
+			//e imprimirá el mensaje de que son anagrama
+			System.out.println("Las palabras son anagrama una de otra");
+			//si es falso...
 		}else {
-			
+			//mensaje de no anagrama
+			System.out.println("Las palabras no son anagrama");
 		}
-		
+		//cerramos escaner
+		sc.close();
 	}
 	//función que compruebe que son anagramas
 	static boolean anagrama(String palabra1,String palabra2) {
-		boolean anagrama=false;
+		//booleano anagrama
+		boolean anagrama=true;
+		//variable índice
+		int ind=0;
+		//while que se repetirá mientra el no termine de recorrer palabra1 o el booleano anagrama sea false
+		while(ind<palabra1.length()&&anagrama) {
+			//cuando no encuentre la palabra devolverá un número negativo y entrará en el if
+			if(palabra1.indexOf(palabra2.charAt(ind))<0) {
+				//demostrando que no es un anagrama
+				anagrama=false;
+			}
+			if(palabra2.indexOf(palabra1.charAt(ind))<0) {
+				//demostrando que no es un anagrama
+				anagrama=false;
+			}
+			//incrementamos el índice
+			ind++;
+		}
 		
 		//devolvemos booleano anagrama
 		return anagrama;
